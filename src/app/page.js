8c1a01bd3e9d2b3236e16c17070bcb45f675e26e
Anish Kumar Singh <anishsingh74850@gmail.com>
2024-09-08 00:42:20 +0530

    added footer

--- src/app/page.js
+++ src/app/page.js
@@ -396,6 +396,7 @@ export default function StudentScheduleManager() {
           </div>
         </TabsContent>
       </Tabs>
+      <footer><p className='text-center text-gray-500 py-2'>Created by Anish Kumar Singh</p></footer>
     </div>
   )
 }
