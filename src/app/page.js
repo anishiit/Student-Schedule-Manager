c9c64a399e2563cd2f22ea0f30c23c12d0179fba
Anish Kumar Singh <149091327+anishiit@users.noreply.github.com>
2025-08-12 00:08:42 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -801,7 +801,6 @@ export default function StudentScheduleManager() {
           </div>
         </div>
       )}
-      <footer><p className='text-center text-gray-500 py-2'>Created by Anish Kumar Singh</p></footer>
     </div>
   )
 }
